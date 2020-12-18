250a15c8433dcc81d01b67bf6d0b1e8bb15eb3cf
Razvan Gututui <razvan.gututui10@gmail.com>
2020-12-18 14:05:44 +0200

    restructuring

--- README.md
+++ README.md
@@ -10,11 +10,4 @@ razvan.gututui@city.ac.uk
 
 On every commit, we will add a short description of work done, for each individual
 
-work done by:
-Radu and Razvan via screensharing
-- read data from file
-- reshuffle
-- dropped columns for unwated data
-- filter data by covid_res
-- drop more columns
-- sort by age
+work done by Radu and Razvan via screensharing
--- cnn.py
+++ cnn.py
@@ -1,27 +1,18 @@
-from sklearn.datasets import load_digits
-import tensorflow as tf
 import os
-import matplotlib.pyplot as plt
-import numpy as np
+
 import tensorflow.keras.utils
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-import io
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import PIL
-from PIL import Image
+
+import matplotlib.pyplot as plt
+
 import numpy as np
-from numpy import save
 from numpy import load
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 # load array
 X_set = load('data_set_images.npy', allow_pickle=True)
@@ -33,70 +24,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_set, y_set, test_size=0.20, random_state=42)
 
 num_classes = y_train.shape[1]
-
-
-# UNOPTIMIZED CNN
-# model = Sequential()
-# model.add(Conv2D(64, kernel_size=(4, 4),
-#                  strides=1, padding='same', input_shape=(100, 100, 3)))
-# model.add(Activation('relu'))
-# model.add(Conv2D(32, (3, 3)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(64, (3, 3), padding='same'))
-# model.add(Activation('relu'))
-# model.add(Conv2D(64, (3, 3)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# model.add(Flatten())
-# model.add(Dense(128))
-# model.add(Activation('relu'))
-
-# model.add(Dense(num_classes))
-# model.add(Activation('softmax'))
-
-# model.compile(loss='categorical_crossentropy',
-#               optimizer='adam',
-#               metrics=['accuracy'])
-
-# print("\n")
-# model.summary()
-
-
-# # X_train_t = torch.from_numpy(X_train)
-
-# history = model.fit(X_train, y_train, verbose=2, epochs=10)
-
-# # make predictions (will give a probability distribution)
-# pred_hot = model.predict(X_test)
-# # now pick the most likely outcome
-# pred = np.argmax(pred_hot, axis=1)
-# y_compare = np.argmax(y_test, axis=1)
-# # calculate accuracy
-# score = metrics.accuracy_score(y_compare, pred)
-
-# print("Accuracy score: {}".format(score))
-
-# # print(pred_hot[:5])
-# # print(pred)
-
-# # Plot training & validation loss values
-# plt.plot(history.history['loss'])
-# plt.title('Model loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-# plt.legend(['Train'], loc='upper left')
-# plt.show()
-
-# mat = confusion_matrix(pred, y_compare)
-
-# # using seaborn
-# sns.heatmap(mat, square=True, annot=True, cbar=False)
-# plt.xlabel('predicted value')
-# plt.ylabel('true value')
-
-# plt.show()
 
 # OPTIMIZED CNN
 
@@ -179,45 +106,7 @@ plt.ylabel('Accuracy')
 plt2.legend(['Accuracy'], loc='upper center')
 plt.show()
 
-# Save model and weights
-model_path = os.path.join("", "saved_model")
-model.save(model_path)
-print('Saved trained model at %s ' % model_path)
-
-# # SVMs
-
-# # method to plot confusion matrices
-# # def plot_confusion_matrix(cm, names, title='Confusion matrix', cmap=plt.cm.gray):
-# #     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-# #     plt.title(title)
-# #     plt.colorbar(fraction=0.05)
-# #     tick_marks = np.arange(len(names))
-# #     plt.xticks(tick_marks, names, rotation=45)
-# #     plt.yticks(tick_marks, names)
-# #     plt.tight_layout()
-# #     plt.ylabel('True label')
-# #     plt.xlabel('Predicted label')
-
-
-# # todo: FIX SVM LINE 243
-
-# # build a multiclass SVM 'ovo' for one-versus-one, and
-# # fit the data
-# multi_svm = SVC(gamma='scale', decision_function_shape='ovo')
-# multi_svm.fit(X_train.reshape(len(X_train), -2), y_train)
-
-# y_pred = multi_svm.predict(X_test)
-
-# # put the results into a DataFrame and print side-by-side
-# output = pd.DataFrame(data=np.c_[y_test, y_pred])
-# print(output)
-
-# # calculate accuracy score and print
-# print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
-
-# # find the confusion matrix, normalise and print
-# # cm = confusion_matrix(y_test, y_pred)
-# # np.set_printoptions(precision=2)
-# # cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-# # print('Normalized confusion matrix')
-# # print(cm_normalized)
+# # Save model and weights
+# model_path = os.path.join("", "saved_model")
+# model.save(model_path)
+# print('Saved trained model at %s ' % model_path)
